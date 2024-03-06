ba7c3a35599548a6fdee32bbafa9f687dab368dd
ChrisHead86 <125095208+ChrisHead86@users.noreply.github.com>
2024-03-05 17:28:23 -0800

    Create main.cpp

---
+++ Homework1/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  CS32Homework1
+//
+//  Created by Chris Head on 7/4/22.
+//
+
+#include "LinkedList.h"
+
+int main() {
+    LinkedList list;
+    LinkedList list2;
+    LinkedList list3;
+    list.insertToFront("d");
+    list.insertToFront("c");
+    list.insertToFront("b");
+    list.insertToFront("a");
+    list2.insertToFront("hi");
+    list2.insertToFront("hello");
+    list2.insertToFront("gduede");
+    list2.insertToFront("andowind");
+    
+}
